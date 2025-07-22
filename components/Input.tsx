7a93eae3eb305fdabe7161d098e9241331da425a
Matheusy1997 <matheusmachado.dev@gmail.com>
2025-07-21 21:10:58 -0300

    implementação da logica do formulario

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,16 +1,35 @@
-export default function Input() {
+import { FormEvent } from "react"
+
+interface InputProps {
+    onSave: () => void
+    changeName: (name: string) => void
+    changeEmail: (name: string) => void
+    changePassword: (name: number) => void
+    changeConfirmPassword: (name: number) => void
+    currentNameValue:string
+    currentEmailValue:string
+    currentPasswordValue:number
+    currentConfirmPasswordValue:number
+}
+export default function Input(props: InputProps) {
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        props.onSave()
+    }
   return (
-    <div className="flex flex-col w-md border rounded p-5 shadow-2xl">
+    <div className="flex flex-col w-md p-5 shadow-md">
       <h1 className=" font-medium text-3xl text-center mb-9">
         Create your account
       </h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col font-medium text-xl">
           <label htmlFor="inName">Name</label>
           <input
+            value={props.currentNameValue}
+            onChange={e => props.changeName(e.target.value)}
             className=" border border-zinc-200 p-2.5 w-full my-2.5
                     focus:outline-none
-                    focus:border-blue-400 shadow-2xs
+                    focus:border-blue-400
                     shadow-sm"
             type="text"
             name="name"
@@ -22,9 +41,11 @@ export default function Input() {
         <div className="flex flex-col font-medium text-xl">
           <label htmlFor="inEmail">Email Address</label>
           <input
+          value={props.currentEmailValue}
+          onChange={e => props.changeEmail(e.target.value)}
             className=" border border-zinc-200 p-2.5 w-full my-2.5
                     focus:outline-none
-                    focus:border-blue-400 shadow-2xs
+                    focus:border-blue-400
                     shadow-sm"
             type="email"
             name="email"
@@ -36,14 +57,34 @@ export default function Input() {
         <div className="flex flex-col font-medium text-xl">
           <label htmlFor="inPassword">Password</label>
           <input
+          value={props.currentPasswordValue === 0 ? 
+            '':props.currentPasswordValue}
+          onChange={e => props.changePassword(Number(e.target.value))}
             className=" border border-zinc-200 p-2.5 w-full my-2.5
                     focus:outline-none
-                    focus:border-blue-400 shadow-2xs
+                    focus:border-blue-400
                     shadow-sm"
             type="password"
             name="password"
             id="inPassword"
             placeholder="Password"
+            required
+          />
+        </div>
+        <div className="flex flex-col font-medium text-xl">
+          <label htmlFor="inPassword">Confirm Password</label>
+          <input
+          value={props.currentConfirmPasswordValue == 0 ?
+            '':props.currentConfirmPasswordValue}
+          onChange={e => props.changeConfirmPassword(Number(e.target.value))}
+            className=" border border-zinc-200 p-2.5 w-full my-2.5
+                    focus:outline-none
+                    focus:border-blue-400
+                    shadow-sm"
+            type="password"
+            name="password"
+            id="inPassword"
+            placeholder="Confirm Password"
             required
           />
         </div>
@@ -54,9 +95,8 @@ export default function Input() {
             Terms and Conditions
           </a>
         </div>
-        <div>
-            <button className="w-full bg-blue-600 text-2xl text-white font-light p-2 cursor-pointer hover:bg-blue-700 transition mb-8">Create Free Account</button>
-        </div>
+
+        <button type="submit" className="w-full bg-blue-600 text-2xl text-white font-light p-2 cursor-pointer hover:bg-blue-700 transition mb-8">Create Free Account</button>
       </form>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+'use client'
 // import Image from "next/image";
 import Input from "../../components/Input";
+import { useState } from "react";
 
 export default function Home() {
+  const [currentName, setCurrentName] = useState<string>('')
+  const [currentEmail, setCurrentEmail] = useState<string>('')
+  const [currentPassword, setCurrentPassword] = useState<number>(0)
+  const [currentConfirmPassword, setcurrentConfirmPassword] = useState<number>(0)
+
+  function handleForm() {
+    if(currentPassword === currentConfirmPassword) {
+      alert('account successfully registered')
+      console.log(`Nome: ${currentName} Email: ${currentEmail} Password: ${currentPassword}`)
+      setCurrentName('')
+      setCurrentEmail('')
+      setCurrentPassword(0)
+      setcurrentConfirmPassword(0)
+    } else {
+      alert('confirmation password must be the same as the first password')
+    }
+  }
+
   return (
     <main className="h-screen w-screen flex justify-center items-center">
-      <Input>
-      </Input>
+      <Input onSave={handleForm} changeName={setCurrentName} changeEmail={setCurrentEmail} changePassword={setCurrentPassword} changeConfirmPassword={setcurrentConfirmPassword} currentNameValue={currentName} currentEmailValue={currentEmail} currentPasswordValue={currentPassword} currentConfirmPasswordValue={currentConfirmPassword}/>
+      
     </main>
   );
 }
